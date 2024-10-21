22a90a9c00f1e1662ff495971b09e61bafda028e
Ihor Kohut <ihor.o.kohut@gmail.com>
2024-10-21 09:31:21 +0100

    Changes to webserver to habdle lambda events

--- server.py
+++ server.py
@@ -1,25 +1,22 @@
 import socket
-from http.server import SimpleHTTPRequestHandler, HTTPServer
+import json
 
-# Function to get the server's IP address
 def get_ip_address():
     hostname = socket.gethostname()
     return socket.gethostbyname(hostname)
 
-class MyHandler(SimpleHTTPRequestHandler):
-    def do_GET(self):
-        ip_address = get_ip_address()
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(f"My IP address is: {ip_address}".encode())
+def lambda_handler(event, context):
+    # Process the event (HTTP GET request in this case)
+    ip_address = get_ip_address()
 
-# Run the server on port 3000
-def run(server_class=HTTPServer, handler_class=MyHandler, port=80):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print(f"Server running on port {port}...")
-    httpd.serve_forever()
+    # Create the response
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "text/html"
+        },
+        "body": f"My IP address is: {ip_address}"
+    }
 
-run()
+    return response
 
